28169224d5e8937d6b579cb5745b0f516057c377
Miguel Ruelas <ruelasjacobo@gmail.com>
2018-09-09 16:44:19 -0400

    Simple C Sharp Excel to SQL Program

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Import
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.itemsImpOutLabel = new System.Windows.Forms.Label();
+            this.countImportLabel = new System.Windows.Forms.Label();
+            this.newItemLabel = new System.Windows.Forms.Label();
+            this.countNewItemLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(333, 572);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Import";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputLabel.Location = new System.Drawing.Point(13, 498);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(301, 21);
+            this.outputLabel.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(727, 482);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // itemsImpOutLabel
+            // 
+            this.itemsImpOutLabel.AutoSize = true;
+            this.itemsImpOutLabel.Location = new System.Drawing.Point(12, 529);
+            this.itemsImpOutLabel.Name = "itemsImpOutLabel";
+            this.itemsImpOutLabel.Size = new System.Drawing.Size(79, 13);
+            this.itemsImpOutLabel.TabIndex = 3;
+            this.itemsImpOutLabel.Text = "Items Imported:";
+            // 
+            // countImportLabel
+            // 
+            this.countImportLabel.AutoSize = true;
+            this.countImportLabel.Location = new System.Drawing.Point(100, 529);
+            this.countImportLabel.Name = "countImportLabel";
+            this.countImportLabel.Size = new System.Drawing.Size(0, 13);
+            this.countImportLabel.TabIndex = 4;
+            // 
+            // newItemLabel
+            // 
+            this.newItemLabel.AutoSize = true;
+            this.newItemLabel.Location = new System.Drawing.Point(13, 546);
+            this.newItemLabel.Name = "newItemLabel";
+            this.newItemLabel.Size = new System.Drawing.Size(60, 13);
+            this.newItemLabel.TabIndex = 5;
+            this.newItemLabel.Text = "New Items:";
+            // 
+            // countNewItemLabel
+            // 
+            this.countNewItemLabel.AutoSize = true;
+            this.countNewItemLabel.Location = new System.Drawing.Point(100, 546);
+            this.countNewItemLabel.Name = "countNewItemLabel";
+            this.countNewItemLabel.Size = new System.Drawing.Size(0, 13);
+            this.countNewItemLabel.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 602);
+            this.Controls.Add(this.countNewItemLabel);
+            this.Controls.Add(this.newItemLabel);
+            this.Controls.Add(this.countImportLabel);
+            this.Controls.Add(this.itemsImpOutLabel);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label itemsImpOutLabel;
+        private System.Windows.Forms.Label countImportLabel;
+        private System.Windows.Forms.Label newItemLabel;
+        private System.Windows.Forms.Label countNewItemLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace Import
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void GetFileName(out string selectedFile) //Move to button1_Click if not autorun on open.
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = "Z:\\";  //Drive letter of initial directory
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK) //Select the file to import
+            {
+                selectedFile = openFileDialog1.FileName;
+                return;
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid Filename");
+            }
+            selectedFile = null;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+
+        public void ImportDataFromExcel(string excelFilePath)
+        {
+            //declare variables - edit these based on your particular situation 
+            string ssqltable = "sampleTable"; //Change the table name to your needs
+            // make sure your sheet name is correct, here sheet name is sheet1, so you can change your sheet name if have    different 
+            string myexceldataquery = "select ASIN, DESCRIPTION, QTY, PRICE, [AZ SELLING PRICE], [SALES RANK], RATING from [Products$]"; //Excel query
+            try
+            {
+                //create our connection strings 
+
+                string sexcelconnectionstring = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + excelFilePath + ";Extended Properties=Excel 12.0;";  //Excel connection string
+        
+                //Server, Database, User login string
+				string ssqlconnectionstring = "Data Source = SQLSRV\\SQLEXPRESS; Initial Catalog = DBNAME; Persist Security Info = True; User ID = USERID; Password =PASSWORD";
+                //execute a query to erase any previous data from our destination table 
+                string sclearsql = "delete from " + ssqltable;   //Create the command string
+                SqlConnection sqlconn = new SqlConnection(ssqlconnectionstring); //Create the connection object
+                SqlCommand sqlcmd = new SqlCommand(sclearsql, sqlconn);          //Create the query command objects SqlCommand(command, connection)
+                
+				try //Try to connect
+                {
+                    sqlconn.Open();
+                    if (sqlconn.State == ConnectionState.Open)
+                    {
+                        //MessageBox.Show("Sqlconn open");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Connection failed.");
+                    }
+				}
+				catch (SqlException ex)  //Something whent wrong, display error
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+				//Delete all records in table
+                sqlcmd.ExecuteNonQuery(); //Executes a Transact-SQL statement against the connection and returns the number of rows affected.
+                sqlconn.Close(); //Close the connection
+
+                //series of commands to bulk copy data from the excel file into our sql table 
+                OleDbConnection oledbconn = new OleDbConnection(sexcelconnectionstring); //Create excel connection object
+                OleDbCommand oledbcmd = new OleDbCommand(myexceldataquery, oledbconn);   //Excel query object
+                try //attempt to open the excel
+                 {
+                     oledbconn.Open();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message + " oledb");
+                }
+                
+                OleDbDataReader dr = oledbcmd.ExecuteReader();   //Object that reads from Excel
+                SqlBulkCopy bulkcopy = new SqlBulkCopy(ssqlconnectionstring);  //Create Bulk Copy object
+                bulkcopy.DestinationTableName = ssqltable;  
+                while (dr.Read())   //Read from excel and copy to SQL
+                {
+                    bulkcopy.WriteToServer(dr);
+                }
+                dr.Close(); //Close excel reader object
+                oledbconn.Close(); //Close SQL connection
+                outputLabel.Text = "File imported into sql server."; //Display Message
+            }
+            catch (Exception ex)
+            {
+              MessageBox.Show(ex.Message + "Exception Thrown");
+           }
+            fillDataGridView();   //Display imported rows 
+        }
+        private void fillDataGridView()
+        {
+			//Alter to show your own individual data in DataGridView
+			
+			//Optional Run Queries after import 
+			//Change Server, DB, USER and pass
+            string connectionString = "Data Source = SQLSRV\\SQLEXPRESS; Initial Catalog = DBNAME; Persist Security Info = True; User ID = USERID; Password =PASS";   
+			string sql = "DELETE FROM tableName WHERE COLUMN IS NULL SELECT * FROM tableName GO exec sp_sample "; //Sample SQL Query to run after import.
+
+            
+            SqlConnection connection = new SqlConnection(connectionString); //Connection object
+            SqlDataAdapter dataadapter = new SqlDataAdapter(sql, connection); //Execute query
+            
+			//Get info from SQL to fill DataGridView
+			DataSet ds = new DataSet();
+            connection.Open();
+            dataadapter.Fill(ds, "test");
+            connection.Close();
+           
+            dataGridView1.DataSource = ds;
+            dataGridView1.DataMember = "test";
+			
+            SqlCommand command = new SqlCommand("Select Count(COLUMN) from TABLE");
+            command.Connection = connection;
+            connection.Open();
+            int newItemCount = (int)command.ExecuteScalar();
+            connection.Close();
+
+            command = new SqlCommand("Select Count(COLUMN) from TABLE");
+            command.Connection = connection;
+            connection.Open();
+            int importItemCount = (int)command.ExecuteScalar();
+            connection.Close();
+            countNewItemLabel.Text = newItemCount.ToString();
+            countImportLabel.Text = importItemCount.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+			//Uncomment for static path, use double \\ for folders
+            //string path = "Z:\\Folder\\subfolder\\excel.xls";
+            GetFileName(out path);
+            //MessageBox.Show(path);
+            ImportDataFromExcel(path);
+            //this.Close();
+        }
+
+    
+    }
+}
